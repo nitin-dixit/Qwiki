3cb8c00b2a38088f538c85cacc54417481099cd9
Nitin Dixit <nitinajaydixit@gmail.com>
2025-12-24 10:00:00 +0530

    feat(db): add article authorization utilities

---
+++ src/db/authz.ts
@@ -0,0 +1,19 @@
+import { eq } from "drizzle-orm";
+import { db } from "@/db";
+import { articles } from "./schema";
+
+export const authorizeUserToEditArticle = async (
+  loggedInUserId: string,
+  articleId: number,
+) => {
+  const response = await db
+    .select({
+      authorId: articles.authorId,
+    })
+    .from(articles)
+    .where(eq(articles.id, articleId));
+
+  if (!response.length) return false;
+
+  return response[0].authorId === loggedInUserId;
+};
